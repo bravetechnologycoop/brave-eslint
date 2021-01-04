3a8f21d1d0c92d66263f3d2865cba1492fe43914
risatronic <mcr.doig@gmail.com>
2021-01-04 14:48:30 -0800

    Fix syntax in quotes rule

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 module.exports = {
-  "env": {
-      "node": true,
-      "es6": true
+  env: {
+    node: true,
+    es6: true,
   },
-  "extends": "eslint:recommended",
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  "parserOptions": {
-      "ecmaVersion": 2018
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  "rules": {
-      "indent": [
-          "error", 4, {"SwitchCase": 1}
-      ],
-      "linebreak-style": [
-          "error", "unix"
-      ],
-      "func-style": [
-        "error", "declaration"
-      ],
-      "quotes": [
-        "error", "single", {"avoidEscape": true}, {"allowTemplateLiterals": true}
-      ]
-  }
+  rules: {
+    indent: [
+      "error",
+      4,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    "linebreak-style": ["error", "unix"],
+    "func-style": ["error", "declaration"],
+    quotes: [
+      "error",
+      "single",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+  },
 }
